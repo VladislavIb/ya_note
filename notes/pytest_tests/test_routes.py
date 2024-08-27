0541677971a86820c02b3f905e3b5a9385c9de2a
Vladislav Ibraev <vladislavibraev@gmail.com>
2024-08-27 11:16:10 +0500

    create conftest.py create tests for test_routes.py

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -1,11 +1,69 @@
 """Тесты маршрутов."""
+import pytest
+
 from http import HTTPStatus
 
 from django.urls import reverse
 
 
-def test_home_availability_for_anonymous_user(client):
+@pytest.mark.parametrize(
+    'name',
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+def test_home_availability_for_anonymous_user(client, name):
     """Анонимный пользователь видит главную страницу."""
     url = reverse('notes:home')
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    """Авторизованный пользователь видит страницы."""
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK),
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_author(
+    parametrized_client, name, note, expected_status
+):
+    """Автор видит страницы."""
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
+
+
+@pytest.mark.parametrize(
+    'name, args',
+    (
+        ('notes:detail', pytest.lazy_fixture('slug_for_args')),
+        ('notes:edit', pytest.lazy_fixture('slug_for_args')),
+        ('notes:delete', pytest.lazy_fixture('slug_for_args')),
+        ('notes:add', None),
+        ('notes:success', None),
+        ('notes:list', None),
+    ),
+)
+def test_redirects(client, name, args):
+    """Проверка редиректа."""
+    login_url = reverse('users:login')
+    url = reverse(name, args=args)
+    expected_url = f'{login_url}?next={url}'
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.FOUND
+    assert response.url == expected_url
